d2c825f3e33fb2b43d2a4e29526f75bc32c8bdb2
Shubhojit-Mitra <shubhojit.120225@stu.upes.ac.in>
2024-04-15 23:48:16 +0530

    Khushi project to ban gaya..DE padha dena plz 🙃

--- Semester II Project/BankAccount.py
+++ Semester II Project/BankAccount.py
@@ -1,5 +1,7 @@
 import csv
 import os
+import csv
+import datetime
 
 # An Exception class for AgeNotValidError: Age should be greater than or equal to 18.
 class AgeNotValidError(Exception):
@@ -12,12 +14,14 @@ class InsufficientBalanceError(Exception):
 class BankAccount:
     # BankAccount class with the following methods:
     # 1. __init__(self, AccNo: str, InitialAmount: float, OwnerDetails: dict) -> None: Constructor to initialize the account details.
-    # 2. AccountDetails(self) -> None: Method to display the account details.
-    # 3. GetBalance(self) -> None: Method to display the account balance.
-    # 4. Deposit(self, Amount: float) -> None: Method to deposit the amount.
-    # 5. ViableTransaction(self, Amount: float) -> None: Method to check if the transaction is viable.
-    # 6. Withdraw(self, Amount: float) -> None: Method to withdraw the amount.
-    # 7. Transfer(self, Amount: float, Account: 'BankAccount') -> None: Method to transfer the amount to another account.
+    # 2. CreateBankStatements(self) -> None: Method to create the bank statements.
+    # 3. StoreOwnerDetails(self, filename: str) -> None: Method to store the owner details in a CSV file. 
+    # 4. AccountDetails(self) -> None: Method to display the account details.
+    # 5. GetBalance(self) -> None: Method to display the account balance.
+    # 6. Deposit(self, Amount: float) -> None: Method to deposit the amount.
+    # 7. ViableTransaction(self, Amount: float) -> None: Method to check if the transaction is viable.
+    # 8. Withdraw(self, Amount: float) -> None: Method to withdraw the amount.
+    # 9. Transfer(self, Amount: float, Account: 'BankAccount') -> None: Method to transfer the amount to another account.
 
     def __init__(self, AccNo: str, InitialAmount: float, OwnerDetails: dict) -> None:
         self.AccNo = AccNo                  # Account Number of the account. str type.
@@ -32,6 +36,20 @@ class BankAccount:
         # If the age is greater than or equal to 18, then the account is created successfully.
         print("\nCongratulations! Your account has been created successfully! 🥳🥳🥳 🤑🤑🤑")
 
+    def CreateBankStatements(self) -> None:
+        try:
+            directory = os.path.dirname(os.path.abspath(__file__))                         # Get the directory path of the current file.
+            filename = f"{self.AccNo}.csv"                                                 # Create the filename using the account number.
+            # Create the full file path by joining the directory path and the filename.
+            filepath = os.path.join(directory + '/Account_Statements', filename)
+            
+            with open(filepath, 'w', newline='') as file:                                  # Open the CSV file in write mode.
+                writer = csv.writer(file)                                                  # Create a CSV writer object.
+                writer.writerow(['Date-Time', 'Operation', 'To', 'Amount', 'Balance'])     # Write the header row.
+               
+            print(f"\nBank statements have been created successfully! ✅")
+        except Exception as error:
+            print(f"\nError occurred while creating the bank statements: {error} ❌")
     
     # Method to store the owner details in a CSV file.
     def StoreOwnerDetails(self, filename: str) -> None:
@@ -69,9 +87,22 @@ class BankAccount:
     
     # Method to deposit the amount.
     def Deposit(self, Amount: float) -> None:
-        self.Balance += Amount          # Add the amount to the balance.
-        print(f"\n$ {Amount:.2f} has been deposited successfully. ✅")
-        self.GetBalance()               # Display the account balance.
+        self.Balance += Amount                                                  # Add the amount to the balance.
+        try:
+            directory = os.path.dirname(os.path.abspath(__file__))              # Get the directory path of the current file.
+            filename = f"{self.AccNo}.csv"                                      # Create the filename using the account number.
+            # Create the full file path by joining the directory path and the filename.
+            filepath = os.path.join(directory + '/Account_Statements', filename)
+            
+            with open(filepath, 'a', newline='') as file:                       # Open the CSV file in append mode.
+                writer = csv.writer(file)                                       # Create a CSV writer object.
+                # Write the deposit operation.
+                writer.writerow([datetime.datetime.now(), 'Deposit', 'N/A', f"${Amount:.2f}", f"${self.Balance:.2f}"])
+            
+            print(f"\n$ {Amount:.2f} has been deposited successfully. ✅")
+            self.GetBalance()                                                   # Display the account balance.
+        except Exception as error:
+            print(f"\nError occurred while depositing the amount: {error} ❌")
 
     # Method to check if the transaction is viable.
     def ViableTransaction(self, Amount: float) -> None:
@@ -84,10 +115,19 @@ class BankAccount:
     def Withdraw(self, Amount: float) -> None:
         # Check if the transaction is viable.
         try:
-            self.ViableTransaction(Amount)      # Check if the transaction is viable.
-            self.Balance -= Amount              # Subtract the amount from the balance.
+            self.ViableTransaction(Amount)                                      # Check if the transaction is viable.
+            self.Balance -= Amount                                              # Subtract the amount from the balance.
+            directory = os.path.dirname(os.path.abspath(__file__))              # Get the directory path of the current file.
+            filename = f"{self.AccNo}.csv"                                      # Create the filename using the account number.
+            # Create the full file path by joining the directory path and the filename.
+            filepath = os.path.join(directory + '/Account_Statements', filename)
+            
+            with open(filepath, 'a', newline='') as file:                       # Open the CSV file in append mode.
+                writer = csv.writer(file)                                       # Create a CSV writer object.
+                # Write the deposit operation.
+                writer.writerow([datetime.datetime.now(), 'Withdraw', 'N/A', f"${Amount:.2f}", f"${self.Balance}"])
             print(f"\n$ {Amount:.2f} has been withdrawn successfully. ✅")
-            self.GetBalance()                   # Display the account balance.
+            self.GetBalance()                                                   # Display the account balance.
         # If the transaction is not viable, then InsufficientBalanceError is raised.
         except InsufficientBalanceError as error:
             print(f"\nWithdraw interrupted: {error} ❌")
@@ -96,11 +136,20 @@ class BankAccount:
     def Transfer(self, Amount: float, Account: 'BankAccount') -> None:
         # Check if the transaction is viable.
         try:
-            self.ViableTransaction(Amount)      # Check if the transaction is viable.
-            self.Balance -= Amount              # Subtract the amount from the balance.
-            Account.Balance += Amount           # Add the amount to the balance of the account to which the amount is transferred.
+            self.ViableTransaction(Amount)                                      # Check if the transaction is viable.
+            self.Balance -= Amount                                              # Subtract the amount from the balance.
+            Account.Balance += Amount                                           # Add the amount to the balance of the account to which the amount is transferred.
+            directory = os.path.dirname(os.path.abspath(__file__))              # Get the directory path of the current file.
+            filename = f"{self.AccNo}.csv"                                      # Create the filename using the account number.
+            # Create the full file path by joining the directory path and the filename.
+            filepath = os.path.join(directory + '/Account_Statements', filename)
+            
+            with open(filepath, 'a', newline='') as file:                       # Open the CSV file in append mode.
+                writer = csv.writer(file)                                       # Create a CSV writer object.
+                # Write the deposit operation.
+                writer.writerow([datetime.datetime.now(), 'Transfer', f"{Account.AccNo}", f"${Amount:.2f}", f"{self.Balance}"])
             print(f"\n$ {Amount:.2f} has been transferred successfully. ✅")
-            self.GetBalance()                   # Display the account balance.
+            self.GetBalance()                                                   # Display the account balance.
         # If the transaction is not viable, then InsufficientBalanceError is raised.
         except InsufficientBalanceError as error:
             print(f"\nTransfer interrupted: {error} ❌")
@@ -114,17 +163,30 @@ class BankAccount:
 # Method Overriding: If the amount deposited is greater than or equal to 1000, then 5% of the amount is added to the balance.
 class RewardsAccount(BankAccount):
     def __init__(self, AccNo: str, InitialAmount: float, OwnerDetails: dict) -> None:
-        super().__init__(AccNo, InitialAmount, OwnerDetails)    # Call the constructor of the parent class.
-        self.AccType = "Rewards"                                # Account Type of the account.
-        self.DepositBenefits = 0.05                             # Deposit Benefits of the account.
+        super().__init__(AccNo, InitialAmount, OwnerDetails)                    # Call the constructor of the parent class.
+        self.AccType = "Rewards"                                                # Account Type of the account.
+        self.DepositBenefits = 0.05                                             # Deposit Benefits of the account.
 
 
     def Deposit(self, Amount: float) -> None:
-        self.Balance += Amount                                  # Add the amount to the balance.    
-        if Amount >= 1000:                                      # If the amount deposited is greater than or equal to 1000.
-            self.Balance += (Amount * self.DepositBenefits)     # Add 8% of the amount to the balance.
-        print(f"\n$ {Amount:.2f} has been deposited successfully. ✅")
-        self.GetBalance()                                       # Display the account balance.
+        self.Balance += Amount                                                  # Add the amount to the balance.    
+        if Amount >= 1000:                                                      # If the amount deposited is greater than or equal to 1000.
+            self.Balance += (Amount * self.DepositBenefits)                     # Add 8% of the amount to the balance.
+        try:
+            directory = os.path.dirname(os.path.abspath(__file__))              # Get the directory path of the current file.
+            filename = f"{self.AccNo}.csv"                                      # Create the filename using the account number.
+            # Create the full file path by joining the directory path and the filename.
+            filepath = os.path.join(directory + '/Account_Statements', filename)
+            
+            with open(filepath, 'a', newline='') as file:                       # Open the CSV file in append mode.
+                writer = csv.writer(file)                                       # Create a CSV writer object.
+                # Write the deposit operation.
+                writer.writerow([datetime.datetime.now(), 'Deposit', 'N/A', f"${Amount:.2f}", f"${self.Balance:.2f}"])
+            
+            print(f"\n$ {Amount:.2f} has been deposited successfully. ✅")
+            self.GetBalance()               # Display the account balance.
+        except Exception as error:
+            print(f"\nError occurred while depositing the amount: {error} ❌")  # Display the account balance.
 
 # A subclass SavingsAccount of the RewardsAccount class.
 # SavingsAccount class with the following methods:
@@ -141,11 +203,20 @@ class SavingsAccount(RewardsAccount):
     def Withdraw(self, Amount: float) -> None:
         # Check if the transaction is viable.
         try:
-            self.ViableTransaction(Amount)                      # Check if the transaction is viable.
-            self.Balance -= Amount                              # Subtract the amount from the balance.
-            self.Balance -= (Amount * self.WithdrawFee)         # Subtract the withdrawal fee from the balance.
+            self.ViableTransaction(Amount)                                      # Check if the transaction is viable.
+            self.Balance -= Amount                                              # Subtract the amount from the balance.
+            self.Balance -= (Amount * self.WithdrawFee)                         # Subtract the withdrawal fee from the balance.
+            directory = os.path.dirname(os.path.abspath(__file__))              # Get the directory path of the current file.
+            filename = f"{self.AccNo}.csv"                                      # Create the filename using the account number.
+            # Create the full file path by joining the directory path and the filename.
+            filepath = os.path.join(directory + '/Account_Statements', filename)
+            
+            with open(filepath, 'a', newline='') as file:                       # Open the CSV file in append mode.
+                writer = csv.writer(file)                                       # Create a CSV writer object.
+                # Write the deposit operation.
+                writer.writerow([datetime.datetime.now(), 'Withdraw', 'N/A', f"${Amount:.2f}", f"${self.Balance}"])
             print(f"\n$ {Amount:.2f} has been withdrawn successfully. ✅")
-            self.GetBalance()                                   # Display the account balance.
+            self.GetBalance()                                                   # Display the account balance.
         # If the transaction is not viable, then InsufficientBalanceError is raised.
         except InsufficientBalanceError as error:
             print(f"\nWithdraw interrupted: {error} ❌")
--- Semester II Project/main.py
+++ Semester II Project/main.py
@@ -29,6 +29,18 @@ owners = [
 
 if __name__ == "__main__":
 
+    """
+    Methods in BankAccount Class:
+
+    1. CreateBankStatements
+    2. StoreOwnerDetails
+    3. AccountDetails
+    4. GetBalance
+    5. Deposit
+    6. Withdraw
+    7. Transfer
+    """
+
     filename = "OwnerDetails.csv"
 
     acc1 = BankAccount("2937400001", 1000, owners[0])
@@ -37,26 +49,30 @@ if __name__ == "__main__":
     acc4 = BankAccount("6875400004", 4000, owners[3])
 
     acc1.StoreOwnerDetails(filename)
+    acc1.CreateBankStatements()
 
-    # acc1.Deposit(200)
+    acc1.Deposit(200)
 
-    # acc1.Withdraw(1000)
+    acc1.Withdraw(1000)
     # acc1.GetBalance()
 
-    # acc1.Transfer(50, acc2)
+    acc1.Transfer(50, acc2)
 
     # acc2.GetBalance()
 
-    # acc3 = RewardsAccount("3349800003", 3000, owners[2])
+    acc3 = RewardsAccount("3349800003", 3000, owners[2])
+    acc3.StoreOwnerDetails(filename)
+    acc3.CreateBankStatements()
 
-    # acc3.AccountDetails()
+
+    acc3.AccountDetails()
     # acc1.AccountDetails()
 
-    # acc3.Deposit(2000) 
+    acc3.Deposit(2000) 
 
-    # acc4 = SavingsAccount("6875400004", 4000, owners[3])
-    # acc4.Deposit(2000)
-    # acc4.Withdraw(1000)
+    acc4 = SavingsAccount("6875400004", 4000, owners[3])
+    acc4.Deposit(2000)
+    acc4.Withdraw(1000)
     
 
 
